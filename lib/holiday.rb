e179b0de969e694ae7f3caed20503db15076e3cd
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 23:52:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,10 +72,9 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.collect { |season, holiday|
-    holiday.collect { |holiday, supply|
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
       holiday if item.include?("BBQ")
-      
-    }
-  }
+    end
+  end.flatten.compact
 end
