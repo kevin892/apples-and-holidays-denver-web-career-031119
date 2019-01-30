b3f1a287af8c2c7e8c0bb322e91d6a8cf35858e0
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 23:24:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each {|key,value|
-return "#{key}: #{data}"
+puts "#{key}: #{data}"
 }
 end
 
