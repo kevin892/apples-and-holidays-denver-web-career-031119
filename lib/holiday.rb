5152525b7e29a6d8304b654737a17bcdb73b95e1
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-30 23:43:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,10 @@ puts "#{season.capitalize}:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holiday_hash.collect {|season, holiday|
+    holiday.collect {|holiday, supply|
+      if supply.include?("BBQ")
+        return holiday
+    }
+  }
 end
