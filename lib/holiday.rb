1a48085fe88d7f82b9a6261c7504c8e2073e74fb
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-31 00:14:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ end
 
     holiday_hash.collect do |season, holiday|
       holiday.collect do |holiday, supplies|
-      if supplies.include?("BBQ")
+      if supplies == "BBQ"
          holiday
       end
       end
